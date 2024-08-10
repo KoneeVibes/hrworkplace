dc36e653a67bf8627903d91272567d7bd3288f73
Konee <104918313+KoneeVibes@users.noreply.github.com>
2024-08-10 07:46:45 +0100

    completed dashboard layout -top nav, side nav, main area all laid out

--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -20,46 +20,58 @@ const sideNavItems = [
 if (sideNavItemsColumn) {
     sideNavItemsColumn.innerHTML = sideNavItems.map((sideNavItem, index) => `
         <div class="row side-nav-item" key="${index}">
-            <i class="${sideNavItem.icon} nav-link-item-icon"></i>
-            <span class="nav-link-item">${sideNavItem.navItem}</span>
-            <i class="fa-solid fa-greater-than drop-down nav-link-item-dropdown"></i>
+            <i class="${sideNavItem.icon} side-nav-link-item-icon"></i>
+            <span class="side-nav-link-item">${sideNavItem.navItem}</span>
+            <i class="fa-solid fa-greater-than drop-down side-nav-link-item-dropdown"></i>
         </div>
     `).join("");
 }
 
 const sideNav = document.querySelector(".side-nav");
 const mainArea = document.querySelector(".main-area");
-const collapsibleNavItems = document.querySelectorAll(".nav-link-item, .nav-link-item-dropdown");
+const collapsibleNavItems = document.querySelectorAll(".side-nav-link-item, .side-nav-link-item-dropdown");
 const hamburgerIcon = document.querySelector('.hamburger-icon');
 const closeIcon = document.querySelector('.close-icon');
+const appLogo = document.querySelector(".logo-img")
 
 function handleHamburgerClick() {
     if (sideNav && mainArea) {
+        const screenSize = window.innerWidth;
         const isHidden = sideNav.classList.toggle("hide-element");
-        mainArea.style.display = isHidden ? "block" : "none";
+        const maxSideNavWidth = screenSize < 425 ? "-webkit-fill-available" : "16rem";
         if (hamburgerIcon && closeIcon) {
             hamburgerIcon.classList.toggle('show');
             closeIcon.classList.toggle('show');
         }
+        mainArea.style.display = isHidden ? "block" : "none";
+        document.documentElement.style.setProperty('--side-nav-width', maxSideNavWidth);
+        collapsibleNavItems.forEach(item => item.classList.remove("hide-element"));
     }
 }
 
-function handleMouseOver(maxSideNavWidth) {
-    if (collapsibleNavItems && mainArea) {
-        const screenSize = window.innerWidth;
+function handleMouseOver() {
+    const screenSize = window.innerWidth;
+    const maxSideNavWidth = screenSize < 425 ? "-webkit-fill-available" : "16rem";
+    if (mainArea) {
+        appLogo.style.width = "auto";
         collapsibleNavItems.forEach(item => item.classList.remove("hide-element"));
         if (screenSize < 768) {
             mainArea.style.opacity = 0.1;
-            document.documentElement.style.setProperty('--side-nav-width', maxSideNavWidth);
+            mainArea.style.pointerEvents = "none";
         } else if (screenSize <= 1024) {
-            document.documentElement.style.setProperty('--side-nav-width', maxSideNavWidth);
+            mainArea.style.opacity = 1;
+            mainArea.style.pointerEvents = "auto";
         }
+        document.documentElement.style.setProperty('--side-nav-width', maxSideNavWidth);
     }
 }
 
-function handleMouseLeave(minSideNavWidth) {
-    if (mainArea && collapsibleNavItems) {
+function handleMouseLeave() {
+    const minSideNavWidth = "4.25rem";
+    if (mainArea) {
+        appLogo.style.width = "100%";
         mainArea.style.opacity = 1;
+        mainArea.style.pointerEvents = "auto";
         document.documentElement.style.setProperty('--side-nav-width', minSideNavWidth);
         collapsibleNavItems.forEach(item => item.classList.add("hide-element"));
     }
@@ -69,16 +81,23 @@ function collapseNavItems() {
     const screenSize = window.innerWidth;
     const minSideNavWidth = "4.25rem";
     const maxSideNavWidth = screenSize < 425 ? "-webkit-fill-available" : "16rem";
-    if (sideNav && collapsibleNavItems) {
+
+    if (sideNav) {
         sideNav.removeEventListener("mouseover", handleMouseOver);
         sideNav.removeEventListener("mouseleave", handleMouseLeave);
 
         if (425 < screenSize && screenSize <= 1024) {
+            appLogo.style.width = "100%";
+            mainArea.style.display = "block";
             document.documentElement.style.setProperty('--side-nav-width', minSideNavWidth);
             collapsibleNavItems.forEach(item => item.classList.add("hide-element"));
-            sideNav.addEventListener("mouseover", () => handleMouseOver(maxSideNavWidth));
-            sideNav.addEventListener("mouseleave", () => handleMouseLeave(minSideNavWidth));
+            sideNav.addEventListener("mouseover", handleMouseOver);
+            sideNav.addEventListener("mouseleave", handleMouseLeave);
         } else {
+            appLogo.style.width = "auto";
+            mainArea.style.display = screenSize < 425 && closeIcon?.classList.contains("show") ? "none" : "block";
+            mainArea.style.opacity = 1;
+            mainArea.style.pointerEvents = "auto";
             document.documentElement.style.setProperty('--side-nav-width', maxSideNavWidth);
             collapsibleNavItems.forEach(item => item.classList.remove("hide-element"));
         }
